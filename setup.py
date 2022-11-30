961d89dd52f9ef2917c15e93f0204f320c1b9317
Lee Dawon <dawon337@gmail.com>
2022-11-30 22:17:17 +0900

    trivial commit

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name = "dawonflow",
+    name = "dwfw",
     version="0.0.1",
     description = "ML/DL/NLP library for python",
     url = "https://github.com/leadawon/dawonflow.git",
     author="LEEDAWON",
     author_email = "dawon337@gmail.com",
     license = "dawonlicense",
-    packages = ["dawonflow"],
+    packages = ["dwfw"],
     zip_safe = False,
     install_requires = [
         "numpy==1.21.6"
